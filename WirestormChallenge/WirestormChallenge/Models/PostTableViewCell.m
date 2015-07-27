06b4f2dc811a09e2e458784afda4f7ef50003511
Marcelo Perretta <mperretta@e-commla.com>
2015-07-26 21:42:34 -0300

    Added post table cell model to manage data from server

---
+++ WirestormChallenge/WirestormChallenge/Models/PostTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.m
+//  WirestormChallenge
+//
+//  Created by marcelo.perretta@gmail.com on 7/26/15.
+//  Copyright (c) 2015 MAWAPE. All rights reserved.
+//
+
+#import "PostTableViewCell.h"
+
+@implementation PostTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
