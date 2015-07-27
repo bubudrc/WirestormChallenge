cf05df2e7d0391670aae75fffd743a0729872e84
Marcelo Perretta <mperretta@e-commla.com>
2015-07-26 21:17:04 -0300

    Added table to manage posts

---
+++ WirestormChallenge/WirestormChallenge/Controllers/PostsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostsTableViewController.h
+//  WirestormChallenge
+//
+//  Created by marcelo.perretta@gmail.com on 7/26/15.
+//  Copyright (c) 2015 MAWAPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostsTableViewController : UITableViewController
+
+@end
