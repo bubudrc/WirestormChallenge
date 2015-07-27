024683bba7d889dbf3d9ffe8820bf09db1195dfb
Marcelo Perretta <mperretta@e-commla.com>
2015-07-26 21:13:26 -0300

    Added Xcode project

---
+++ WirestormChallenge/WirestormChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WirestormChallenge
+//
+//  Created by marcelo.perretta@gmail.com on 7/26/15.
+//  Copyright (c) 2015 MAWAPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WirestormChallenge/WirestormChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WirestormChallenge
+//
+//  Created by marcelo.perretta@gmail.com on 7/26/15.
+//  Copyright (c) 2015 MAWAPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
